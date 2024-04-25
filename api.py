ff288e09ecf3389a629e50daefe4c76a07188047
Савва <savvmuhin@yandex.ru>
2024-04-26 00:00:44 +0200

    Оригинал

---
+++ api.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html', title='Главная')
+
+@app.route('/projects')
+def projects():
+    return render_template('projects.html', title='Проекты')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html', title='Контакты')
+
+if __name__ == '__main__':
+    app.run(debug=True)
