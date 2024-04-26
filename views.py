7acc7f0cb9abc4611113244d9959942446addafe
Савва <savvmuhin@yandex.ru>
2024-04-26 18:52:10 +0200

    ne last

--- views.py
+++ views.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -18,5 +18,21 @@ def contact():
     return render_template('contact.html', title='Контакты')
 
 
+@app.route('/submit_consultation', methods=['POST'])
+def submit_consultation():
+    # Получение данных из формы
+    first_name = request.form.get('first_name')
+    last_name = request.form.get('last_name')
+    middle_name = request.form.get('middle_name')
+    email = request.form.get('email')
+    purpose = request.form.get('purpose')
+
+    # Логика обработки данных формы
+
+    # Возвращаем сообщение об успешной отправке консультации
+    return render_template('consultation_success.html', title='Консультация отправлена')
+
+
+
 if __name__ == '__main__':
-    app.run(debug=False)
+    app.run(debug=True)
